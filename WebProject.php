badbfc203f9fab2bfe0f456403b3e0a1db1aeed4
ERedEarth99 <eredearth99@gmail.com>
2024-10-25 11:30:37 -0500

    Add files via upload

---
+++ WebProject.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<head>
+<link ref="stylesheet" href="./WebProject.css">
+<body>
+Movie: <?php echo $_GET["Mname"]; ?><br>
+Game: <?php echo $_GET["Gname"]; ?><br>
+Book: <?php echo $_GET["Bname"]; ?>
+
+</body>
+</head>
+</html>
