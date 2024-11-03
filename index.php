ff53971b9afe14918a991883cdac4a44ac9d7747
Aaron Staples <amstaples25@gmail.com>
2024-11-03 17:11:54 -0600

    Attempt at fixing merge issue 1

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php 
 
     //AS - Add in all Functions
@@ -56,34 +56,3 @@
             break;
     }
 ?>
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Bootstrap Site</title>
-  <link ref="stylesheet" href="./">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>    
-</head>
-<body>
-<h1>Project</h1>
-<main>
-    <h1>Menu</h1>
-    <ul>
-        <li>
-            <a href="Games/gamesView.php">Games</a>
-        </li>
-        <li>
-            <a href="Movies/moviesView.php">Movies</a>
-        </li>
-        <li>
-            <a href="Books/booksView.php">Books</a>
-        </li>
-    </ul>
-</main>
-</body>
-</html>
->>>>>>> 203118752855386d063ff22a9f986c397ee78ace
