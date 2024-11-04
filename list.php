c8a57eadef6ba0918ebc60dbe8d9c5e560029251
Aaron Staples <amstaples25@gmail.com>
2024-11-03 18:54:49 -0600

    List.php basic structure and function completed

--- list.php
+++ list.php
@@ -15,43 +15,48 @@ AS - List all tables information and options to interact with the database
     <table>
         <tr>
             <th>Name:</th>
-            <th>Year:</th>
             <th>Author:</th>
+            <th>Year:</th>
             <th>Publisher:</th>
             <th>Rating:</th>
             <th>Genre:</th>
             <th>&nbsp;</th>
         </tr>
-
+        
         <?php foreach ($books as $book) : ?>
-        <tr><?php echo $book['BookName'] ?> </tr>
-        <tr><?php echo $book['Author'] ?> </tr>
-        <tr><?php echo $book['ReleaseYear'] ?> </tr>
-        <tr><?php echo $book['Publisher'] ?> </tr>
-        <tr><?php echo $book['Rating'] ?> </tr>
-        <tr><?php echo $book['BookGenre'] ?> </tr>
-        <tr> <!-- AS - Update Function -->
+        <tr>
+        <td><?php echo $book['BookName'] ?> </td>
+        <td><?php echo $book['Author'] ?> </td>
+        <td><?php echo $book['ReleaseYear'] ?> </td>
+        <td><?php echo $book['Publisher'] ?> </td>
+        <td><?php echo $book['Rating'] ?> </td>
+        <td><?php echo $book['BookGenre'] ?> </td>
+        <td> <!-- AS - Update Function -->
             <form action = "index.php" method = "post">
                 <input type="hidden" name="action" value="update_book">
                     <input type="hidden" name="movie_ID" value="<?php echo $book['BookID'];?>">
                     <input type="submit" value="Update">
             </form>
-        </tr>
-        <tr> <!-- AS - Simple Delete Functionality -->
+        </td>
+        <td> <!-- AS - Simple Delete Functionality -->
             <form action = "index.php" method = "post">
                 <input type="hidden" name="action" value="delete_book">
                     <input type="hidden" name="book_ID" value="<?php echo $book['BookID'];?>">
                     <input type="submit" value="Delete">
             </form>
+        </td>
         </tr>
         <?php endforeach ?>
-
+        </table>
+        <form action="index.php" value="add_book">
+            <input type="submit" value="Add Book">
+        </form>
         <h2>Movies</h2>
     <table>
         <tr>
             <th>Name:</th>
-            <th>Year:</th>
             <th>Director:</th>
+            <th>Year:</th>
             <th>Publisher:</th>
             <th>Rating:</th>
             <th>Genre:</th>
@@ -59,35 +64,39 @@ AS - List all tables information and options to interact with the database
         </tr>
 
         <?php foreach ($movies as $movie) : ?>
-        <tr><?php echo $movie['MovieName'] ?> </tr>
-        <tr><?php echo $movie['Director'] ?> </tr>
-        <tr><?php echo $movie['ReleaseYear'] ?> </tr>
-        <tr><?php echo $movie['Publisher'] ?> </tr>
-        <tr><?php echo $movie['Rating'] ?> </tr>
-        <tr><?php echo $movie['MovieGenre'] ?> </tr>
-        <tr> <!-- AS - Update Function -->
+        <tr>
+        <td><?php echo $movie['MovieName'] ?> </td>
+        <td><?php echo $movie['Director'] ?> </td>
+        <td><?php echo $movie['ReleaseYear'] ?> </td>
+        <td><?php echo $movie['Publisher'] ?> </td>
+        <td><?php echo $movie['Rating'] ?> </td>
+        <td><?php echo $movie['MovieGenre'] ?> </td>
+        <td> <!-- AS - Update Function -->
             <form action = "index.php" method = "post">
                 <input type="hidden" name="action" value="update_movie">
                     <input type="hidden" name="movie_ID" value="<?php echo $movie['MovieID'];?>">
                     <input type="submit" value="Update">
             </form>
-        </tr>
-        <tr> <!-- AS - Simple Delete Functionality -->
+        </td>
+        <td> <!-- AS - Simple Delete Functionality -->
             <form action = "index.php" method = "post">
                 <input type="hidden" name="action" value="delete_movie">
                     <input type="hidden" name="movie_ID" value="<?php echo $movie['MovieID'];?>">
                     <input type="submit" value="Delete">
             </form>
+        </td>
         </tr>
         <?php endforeach ?>
     </table>
-
-    <h2>Movies</h2>
+    <form action="index.php" value="add_movie">
+            <input type="submit" value="Add Movie">
+        </form>
+    <h2>Games</h2>
     <table>
         <tr>
             <th>Name:</th>
-            <th>Year:</th>
             <th>Developer:</th>
+            <th>Year:</th>
             <th>Publisher:</th>
             <th>Rating:</th>
             <th>Genre:</th>
@@ -95,27 +104,32 @@ AS - List all tables information and options to interact with the database
         </tr>
 
         <?php foreach ($games as $game) : ?>
-        <tr><?php echo $game['GameName'] ?> </tr>
-        <tr><?php echo $game['Developer'] ?> </tr>
-        <tr><?php echo $game['ReleaseYear'] ?> </tr>
-        <tr><?php echo $game['Publisher'] ?> </tr>
-        <tr><?php echo $game['Rating'] ?> </tr>
-        <tr><?php echo $game['GameGenre'] ?> </tr>
-        <tr> <!-- AS - Update Function -->
+        <tr>
+        <td><?php echo $game['GameName'] ?> </td>
+        <td><?php echo $game['Developer'] ?> </td>
+        <td><?php echo $game['ReleaseYear'] ?> </td>
+        <td><?php echo $game['Publisher'] ?> </td>
+        <td><?php echo $game['Rating'] ?> </td>
+        <td><?php echo $game['GameGenre'] ?> </td>
+        <td> <!-- AS - Update Function -->
             <form action = "index.php" method = "post">
                 <input type="hidden" name="action" value="update_game">
                     <input type="hidden" name="movie_ID" value="<?php echo $game['GameID'];?>">
                     <input type="submit" value="Update">
             </form>
-        </tr>
-        <tr> <!-- AS - Simple Delete Functionality -->
+        </td>
+        <td> <!-- AS - Simple Delete Functionality -->
             <form action = "index.php" method = "post">
                 <input type="hidden" name="action" value="delete_game">
                     <input type="hidden" name="game_ID" value="<?php echo $game['GameID'];?>">
                     <input type="submit" value="Delete">
             </form>
+        </td>
         </tr>
         <?php endforeach ?>
     </table>
+    <form action="index.php" value="add_game">
+            <input type="submit" value="Add Game">
+        </form>
 </body>
 </html>
