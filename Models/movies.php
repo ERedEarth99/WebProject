cd17470282c03b3070ba543bf718c28a6e7cf437
ERedEarth99 <eredearth99@gmail.com>
2024-11-03 18:25:17 -0600

    Update movies.php
    Updated the movies.php

--- Models/movies.php
+++ Models/movies.php
@@ -13,7 +13,7 @@ function get_media_by_category($category_id) {
     $statement->closeCursor();
     return $media;
 }
-//allows you to grab a product to update it
+//allows you to grab a product to update it //AS - This does not update. it just gets.
 function get_product($movie_id) {
     global $db;
     $query = 'SELECT * FROM movies
@@ -35,17 +35,37 @@ function delete_media($movie_id) {
     $statement->execute();
     $statement->closeCursor();
 }
-//allows you to add a movie/game/book
-function add_product($id, $release, $name, $director, $publisher, $rating, $genre) {
+//allows you to add a movie/game/book //AS - Do not ever let a user edit the ID field.
+                                      //I changed this to UPDATE as thats the only thing you should use ID for.
+function update_product($movie_id, $release, $name, $director, $publisher, $rating, $genre) {
+    global $db;
+    $query = 'UPDATE movies
+              SET MovieName = :movie_name, ReleaseYear = :release, Rating = :rating, Director = :director, Publisher = :publisher, MovieGenre = :genre
+              WHERE MovieID = :movie_id';
+    
+    $statement = $db->prepare($query);
+    $statement->bindValue(':movie_id', $movie_id);
+    $statement->bindValue(':release', $release);
+    $statement->bindValue(':movie_name', $name);
+    $statement->bindValue(':rating', $rating);
+    $statement->bindValue(':director', director);
+    $statement->bindValue(':publisher', $publisher);
+    $statement->bindValue(':genre', $genre);
+    $statement->execute();
+    $statement->closeCursor();
+}
+
+//AS - Took the old add_product and removed the ID field. Leaving the ID field blank will auto generate an ID.
+function add_product($release, $name, $director, $publisher, $rating, $genre) {
     global $db;
     $query = 'INSERT INTO books
-                 (MovieID, MovieName, ReleaseYear, Rating, Director, Publisher, MovieGenre)
+                 (MovieName, ReleaseYear, Rating, Director, Publisher, MovieGenre)
               VALUES
-                 (:id, :name_, :release, :rating, :director, :publisher, :genre)';
+                 (:movie_name, :release, :rating, :director, :publisher, :genre)';
     $statement = $db->prepare($query);
-    $statement->bindValue(':id', $id);
+    $statement->bindValue(':movie_id', $movie_id);
     $statement->bindValue(':release', $release);
-    $statement->bindValue(':name_', $name);
+    $statement->bindValue(':movie_name', $name);
     $statement->bindValue(':rating', $rating);
     $statement->bindValue(':director', $director);
     $statement->bindValue(':publisher', $publisher);
