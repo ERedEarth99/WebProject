43b15a0662d519d3507cff12630fd402b93b7370
ERedEarth99 <eredearth99@gmail.com>
2024-11-03 18:19:13 -0600

    Update games.php
    Updated games.php

--- Models/games.php
+++ Models/games.php
@@ -13,11 +13,12 @@ function get_media_by_category($category_id) {
     $statement->closeCursor();
     return $media;
 }
-//allows you to grab a product to update it
+
+//allows you to grab a product to update it //AS - This does not update. it just gets.
 function get_product($game_id) {
     global $db;
     $query = 'SELECT * FROM games
-              WHERE GameID = :game_id';
+              WHERE BookID = :game_id';
     $statement = $db->prepare($query);
     $statement->bindValue(':game_id', $game_id);
     $statement->execute();
@@ -35,19 +36,39 @@ function delete_media($game_id) {
     $statement->execute();
     $statement->closeCursor();
 }
-//allows you to add a movie/game/book
-function add_product($id, $release, $name, $director, $publisher, $rating, $genre) {
+//allows you to add a movie/game/book //AS - Do not ever let a user edit the ID field.
+                                      //I changed this to UPDATE as thats the only thing you should use ID for.
+function update_product($game_id, $release, $name, $developer, $publisher, $rating, $genre) {
+    global $db;
+    $query = 'UPDATE games
+              SET GameName = :game_name, ReleaseYear = :release, Rating = :rating, Developer = :developer, Publisher = :publisher, GameGenre = :genre
+              WHERE GameID = :game_id';
+    
+    $statement = $db->prepare($query);
+    $statement->bindValue(':game_id', $game_id);
+    $statement->bindValue(':release', $release);
+    $statement->bindValue(':game_name', $name);
+    $statement->bindValue(':rating', $rating);
+    $statement->bindValue(':developer', developer);
+    $statement->bindValue(':publisher', $publisher);
+    $statement->bindValue(':genre', $genre);
+    $statement->execute();
+    $statement->closeCursor();
+}
+
+//AS - Took the old add_product and removed the ID field. Leaving the ID field blank will auto generate an ID.
+function add_product($release, $name, $developer, $publisher, $rating, $genre) {
     global $db;
     $query = 'INSERT INTO books
-                 (GameID, GameName, ReleaseYear, Rating, Developer, Publisher, GameGenre)
+                 (GameName, ReleaseYear, Rating, Developer, Publisher, GameGenre)
               VALUES
-                 (:id, :name_, :release, :rating, :developer, :publisher, :genre)';
+                 (:book_name, :release, :rating, :author, :publisher, :genre)';
     $statement = $db->prepare($query);
-    $statement->bindValue(':id', $id);
+    $statement->bindValue(':game_id', $game_id);
     $statement->bindValue(':release', $release);
-    $statement->bindValue(':name_', $name);
+    $statement->bindValue(':game_name', $name);
     $statement->bindValue(':rating', $rating);
-    $statement->bindValue(':developer', $developer);
+    $statement->bindValue(':developer', developer);
     $statement->bindValue(':publisher', $publisher);
     $statement->bindValue(':genre', $genre);
     $statement->execute();
